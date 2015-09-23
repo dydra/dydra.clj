964baf59110deef5775909a5b107d90b3135c24c
Arto Bendiken <arto@bendiken.net>
2015-09-23 15:57:21 +0000

    add init-ns repl option

--- project.clj
+++ project.clj
@@ -3,15 +3,17 @@
   :url "http://dydra.com"
   :license {:name "Unlicense"
             :url "http://unlicense.org"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.dydra/dydra-ndk "0.0.1"]
-                 ;; pick
-                 [org.openrdf.sesame "2.7.3"]
-                ]
   ;; locate the dydra java ndk in a local repository
   :repositories [;; ["dydrarepo" "file:///development/lib"]
                  ["dydrarepo" "file:///home/arto/lib"]
                  ]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.slf4j/slf4j-jdk14 "1.7.5"]
+                 ;; pick
+                 [org.openrdf.sesame "2.7.3"]
+                 [com.dydra/dydra-ndk "0.0.1"]
+                ]
+  :repl-options {:init-ns dydra.core}
   :min-lein-version "2.5.1"
   :main dydra.core
   :aot :all
